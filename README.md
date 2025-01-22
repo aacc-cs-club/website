509b891fc1b471ba2aa4f6e048d66b0e8f7efc72
Urdons <urdons101@gmail.com>
2025-01-21 21:50:42 -0500

    Added Tailwind and Proper Page Content

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Visit this URL to access the website: https://aacc-cs-club.github.io/website/
+
+Want to modify the website? use ```npx tailwindcss -i ./src/input.css -o ./src/output.css --watch``` to start the tailwind compiler!
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,ts}"],
+  theme: {
+    extend: {},
+    colors: {
+      void: "var(--void)",
+      background: "var(--background)",
+      border: "var(--border)",
+      text: {
+        hard: "var(--text-hard)",
+        soft: "var(--text-soft)",
+      },
+      highlight: "#46a6b9",
+    },
+  },
+  plugins: [],
+};
